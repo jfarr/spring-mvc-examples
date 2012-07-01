d33ce11af8da20d97aaecc00e2ed4a6780bfa6df
jfarr <jonothan.farr@gmail.com>
2012-06-30 18:53:02 -0700

    initial revision

---
+++ example-multi-action-controller/src/main/java/examples/controller/BookController.java
@@ -0,0 +1,50 @@
+package examples.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.Validate;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import examples.data.Book;
+import examples.data.Library;
+
+public class BookController extends MultiActionController {
+
+    private Library library;
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
+
+    public ModelAndView addForm(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView("book/edit", "book", new Book());
+    }
+
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Book book = bind(request);
+        library.saveBook(book);
+        return new ModelAndView("redirect:book.form?action=view", "bookId", book.getBookId());
+    }
+
+    private Book bind(HttpServletRequest request) throws Exception {
+        Book book = new Book();
+        bind(request, book);
+        return book;
+    }
+    
+    public ModelAndView view(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView("book/view", "book", getBook(request));
+    }
+
+    public ModelAndView editForm(HttpServletRequest request, HttpServletResponse response) {
+        return new ModelAndView("book/edit", "book", getBook(request));
+    }
+
+    private Book getBook(HttpServletRequest request) {
+        String bookId = request.getParameter("bookId");
+        Validate.notNull(bookId, "missing bookId parameter");
+        return library.getBook(Integer.parseInt(bookId));
+    }
+}
---
+++ example-multi-action-controller/src/main/webapp/WEB-INF/jsp/book/edit.jsp
@@ -0,0 +1,53 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<c:choose>
+	<c:when test="${param.bookId == null}">
+		<title>Add Book</title>
+	</c:when>
+	<c:otherwise>
+		<title>Edit Book</title>
+	</c:otherwise>
+</c:choose>
+</head>
+<body>
+<c:choose>
+    <c:when test="${param.bookId == null}">
+        <h2>Add Book</h2>
+    </c:when>
+    <c:otherwise>
+        <h2>Edit Book</h2>
+    </c:otherwise>
+</c:choose>
+<form action="book.form" method="POST">
+<table>
+	<tr>
+		<th>Title</th>
+		<td><input type="text" name="title" value="${book.title}" /></td>
+	</tr>
+	<tr>
+		<th>Author</th>
+		<td><input type="text" name="author" value="${book.author}" /></td>
+	</tr>
+	<tr>
+		<td rowspan="2""><input type="submit" name="submit" value="Save" /></td>
+	</tr>
+</table>
+<c:if test="${book.bookId != null}">
+<input type="hidden" name="bookId" value="<c:out value="${book.bookId}"/>" />
+</c:if> 
+<input type="hidden" name="action" value="save" />
+</form>
+<hr>
+request:
+<c:out value="${param}" />
+<br />
+model:
+<c:out value="${book}" />
+<br />
+</body>
+</html>
