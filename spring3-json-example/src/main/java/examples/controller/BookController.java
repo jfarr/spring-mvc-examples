e6230310703a1b2686146bc651254685007697c3
jfarr <jonothan.farr@gmail.com>
2012-07-01 01:08:24 -0700

    cleanup

--- spring3-json-example/src/main/java/examples/controller/BookController.java
+++ spring3-json-example/src/main/java/examples/controller/BookController.java
@@ -1,8 +1,5 @@
 package examples.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -27,7 +24,7 @@ public class BookController {
     }
 
     @RequestMapping("/")
-    public ModelAndView addForm(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView addForm() {
         return new ModelAndView("book/edit", "book", new Book());
     }
 
