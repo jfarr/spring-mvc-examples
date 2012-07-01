ff9c7dd3aa0aa52791fb13251e40f01848436ebc
jfarr <jonothan.farr@gmail.com>
2012-06-30 23:28:25 -0700

    map the saveJson action to PUT instead of POST

--- spring3-json-example/src/main/java/examples/controller/BookController.java
+++ spring3-json-example/src/main/java/examples/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -30,29 +31,29 @@ public class BookController {
         return new ModelAndView("book/edit", "book", new Book());
     }
 
-    @RequestMapping(params="action=save")
+    @RequestMapping(params = "action=save")
     public ModelAndView save(@ModelAttribute("book") Book book) throws Exception {
         library.saveBook(book);
         return new ModelAndView("redirect:book.form?action=view", "bookId", book.getBookId());
     }
 
-    @RequestMapping(params="action=save", headers="Content-type=application/json")
+    @RequestMapping(params = "action=save", method = RequestMethod.PUT, headers = "Content-type=application/json")
     public ModelAndView saveJson(@RequestBody Book book) throws Exception {
         library.saveBook(book);
         return new ModelAndView("redirect:book.form?action=view", "bookId", book.getBookId());
     }
 
-    @RequestMapping(params="action=view")
+    @RequestMapping(params = "action=view")
     public ModelAndView view(@RequestParam Integer bookId) {
         return new ModelAndView("book/view", "book", library.getBook(bookId));
     }
 
-    @RequestMapping(params="action=view", headers="Accept=application/json")
+    @RequestMapping(params = "action=view", headers = "Accept=application/json")
     public ModelAndView viewJson(@RequestParam Integer bookId) {
         return new ModelAndView("book/view-json", "book", library.getBook(bookId));
     }
-    
-    @RequestMapping(params="action=editForm")
+
+    @RequestMapping(params = "action=editForm")
     public ModelAndView editForm(@RequestParam Integer bookId) {
         return new ModelAndView("book/edit", "book", library.getBook(bookId));
     }
