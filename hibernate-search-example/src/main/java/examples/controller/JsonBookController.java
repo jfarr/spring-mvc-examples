f5e184b23e1df7cd904dd0d624a6a35ad6a0d617
jfarr <jonothan.farr@gmail.com>
2012-08-19 14:24:09 -0700

    return 204 No Content from DELETE

--- hibernate-search-example/src/main/java/examples/controller/JsonBookController.java
+++ hibernate-search-example/src/main/java/examples/controller/JsonBookController.java
@@ -1,11 +1,13 @@
 package examples.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import examples.data.Book;
@@ -35,6 +37,7 @@ public class JsonBookController extends AbstractBookController {
     }
 
     @RequestMapping(value = "/book/{bookId}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int bookId) {
         deleteBook(bookId);
     }
