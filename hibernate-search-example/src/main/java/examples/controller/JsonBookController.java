b4c865ebb519e1abcf90cefa7f5293654e6fd843
jfarr <jonothan.farr@gmail.com>
2012-09-10 01:01:56 -0700

    implemented Server-Side Access Control for Cross-Origin Resource Sharing using an HTTP servlet filter

--- hibernate-search-example/src/main/java/examples/controller/JsonBookController.java
+++ hibernate-search-example/src/main/java/examples/controller/JsonBookController.java
@@ -2,8 +2,6 @@ package examples.controller;
 
 import java.util.ArrayList;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,22 +21,18 @@ public class JsonBookController extends AbstractBookController {
     @RequestMapping(value = "/", method = RequestMethod.GET, headers = "Accept=application/json")
     public ModelAndView list(
             @RequestParam(required = false) Integer firstResult,
-            @RequestParam(required = false) Integer maxResults,
-            HttpServletResponse response) {
-        setResponseHeaders(response);
+            @RequestParam(required = false) Integer maxResults) {
         return new ModelAndView("book/list-json", getBooks(firstResult, maxResults));
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST, headers = "Content-type=application/json")
-    public ModelAndView add(@RequestBody Book book, HttpServletResponse response) {
-        setResponseHeaders(response);
+    public ModelAndView add(@RequestBody Book book) {
         saveBook(book);
         return new ModelAndView("book/view-json", "book", book);
     }
 
     @RequestMapping(value = "/book/{bookId}", method = RequestMethod.PUT, headers = "Content-type=application/json")
-    public ModelAndView update(@PathVariable int bookId, @RequestBody Book book, HttpServletResponse response) {
-        setResponseHeaders(response);
+    public ModelAndView update(@PathVariable int bookId, @RequestBody Book book) {
         book.setBookId(bookId);
         saveBook(book);
         return new ModelAndView("book/view-json", "book", book);
@@ -46,14 +40,12 @@ public class JsonBookController extends AbstractBookController {
 
     @RequestMapping(value = "/book/{bookId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable int bookId, HttpServletResponse response) {
-        setResponseHeaders(response);
+    public void delete(@PathVariable int bookId) {
         deleteBook(bookId);
     }
 
     @RequestMapping(value = "/book/{bookId}", method = RequestMethod.GET, headers = "Accept=application/json")
-    public ModelAndView view(@PathVariable int bookId, HttpServletResponse response) throws NotFoundException {
-        setResponseHeaders(response);
+    public ModelAndView view(@PathVariable int bookId) throws NotFoundException {
         return new ModelAndView("book/view-json", "book", getBook(bookId));
     }
 
@@ -62,18 +54,12 @@ public class JsonBookController extends AbstractBookController {
             @RequestParam(required = false) String contains,
             @RequestParam(required = false) String prefix,
             @RequestParam(required = false) Integer firstResult,
-            @RequestParam(required = false) Integer maxResults,
-            HttpServletResponse response) {
-        setResponseHeaders(response);
+            @RequestParam(required = false) Integer maxResults) {
         if (contains != null) {
             return new ModelAndView("book/list-json", searchBooksByTitle(contains, firstResult, maxResults));
         } else if (prefix != null) {
             return new ModelAndView("book/list-json", searchBooksByTitlePrefix(prefix, firstResult, maxResults));
         }
         return new ModelAndView("book/list-json", "books", new ArrayList<Book>());
-    }
-
-    private void setResponseHeaders(HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Origin", "*");
     }
 }
