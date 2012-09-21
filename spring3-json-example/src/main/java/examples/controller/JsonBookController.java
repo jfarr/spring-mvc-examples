8455f1f3d85777d2c18e966bfcb88ca5648ccc47
Mark Yu <vafada@gmail.com>
2012-09-20 23:38:44 -0400

    1. simplified configuration 2. use @ResponseBody 3. added json method to retrieve library

--- spring3-json-example/src/main/java/examples/controller/JsonBookController.java
+++ spring3-json-example/src/main/java/examples/controller/JsonBookController.java
@@ -1,33 +1,37 @@
 package examples.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import examples.data.Book;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/books")
 public class JsonBookController extends AbstractBookController {
 
     @RequestMapping(value = "/", method = RequestMethod.POST, headers = "Content-type=application/json")
-    public ModelAndView addJson(@RequestBody Book book) throws Exception {
+    public @ResponseBody Book addJson(@RequestBody Book book) throws Exception {
         saveBook(book);
-        return new ModelAndView("book/view-json", "book", book);
+        return book;
     }
 
     @RequestMapping(value = "/book/{bookId}", method = RequestMethod.PUT, headers = "Content-type=application/json")
-    public ModelAndView updateJson(@PathVariable int bookId, @RequestBody Book book) throws Exception {
+    public @ResponseBody Book updateJson(@PathVariable int bookId, @RequestBody Book book) throws Exception {
         book.setBookId(bookId);
         saveBook(book);
-        return new ModelAndView("book/view-json", "book", book);
+        return book;
     }
 
     @RequestMapping(value = "/book/{bookId}", method = RequestMethod.GET, headers = "Accept=application/json")
-    public ModelAndView viewJson(@PathVariable int bookId) throws NotFoundException {
-        return new ModelAndView("book/view-json", "book", getBook(bookId));
+    public @ResponseBody Book viewJson(@PathVariable int bookId) throws NotFoundException {
+        return getBook(bookId);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET, headers = "Accept=application/json")
+    public @ResponseBody Map<Integer, Book> listJson() throws NotFoundException {
+        return library.getBooks();
     }
 }
